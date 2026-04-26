24364818c17ef7a60fa213b1178dd6d628f4b47c
MariaPaz0617 <138548940+MariaPaz0617@users.noreply.github.com>
2026-04-25 21:43:12 -0600

    Importar Client de Apollo en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,31 @@ import VehicleDetail from "./pages/VehicleDetail";
 import Chat from "./pages/Chat";
 import Verify2FA from "./pages/Verify2FA";
 import Navbar from "./components/Navbar";
+import { ApolloProvider } from '@apollo/client';
+
+//importar cliente de Apollo desde el archivo client.js
+import client from './services/client';  
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-100">
-      <Navbar />
+    <ApolloProvider client={client}> 
+      <div className="min-h-screen bg-slate-100">
+        <Navbar />
 
-      <div className="p-6">
-        <Routes>
-          <Route path="/" element={<PublicHome />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/verificar-2fa" element={<Verify2FA />} />
-          <Route path="/mis-vehiculos" element={<GestionarVehicle />} />
-          <Route path="/vehicles/:id" element={<VehicleDetail />} />
-          <Route path="/chat" element={<Chat />} />
-        </Routes>
+        <div className="p-6">
+          <Routes>
+            <Route path="/" element={<PublicHome />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/verificar-2fa" element={<Verify2FA />} />
+            <Route path="/mis-vehiculos" element={<GestionarVehicle />} />
+            <Route path="/vehicles/:id" element={<VehicleDetail />} />
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
 
